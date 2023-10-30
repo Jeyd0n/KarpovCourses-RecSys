a2c78fd5bafb80fa3837eb2d315bfe4bba53f6f7
Jeyd0n <polukhin.vo@icloud.com>
2023-10-30 16:13:48 +0300

    change directory for any files

--- modules/model_loading.py
+++ modules/model_loading.py
@@ -8,6 +8,7 @@ def get_model_path(path: str) -> str:
     return MODEL_PATH
 
 def load_models():
-    model_path = get_model_path("./balanced_model")
+    model_path = get_model_path("../model/balanced_model")
     from_file = CatBoostClassifier()
+
     return from_file.load_model(model_path)
